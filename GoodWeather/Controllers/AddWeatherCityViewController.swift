271d9d415b2b9d54a4af609291dcb5c69525dd44
Henrique Alves Batochi <32850493+halvesbatochi@users.noreply.github.com>
2022-09-07 20:54:40 -0300

    Webservice branch

--- GoodWeather/Controllers/AddWeatherCityViewController.swift
+++ GoodWeather/Controllers/AddWeatherCityViewController.swift
@@ -14,6 +14,21 @@ class AddWeatherCityViewController: UIViewController {
     
     @IBAction func saveCityButtonPressed() {
         
+        if let city = cityNameTextField.text {
+            
+            let weatherURL = URL(string: "https://api.openweathermap.org/data/2.5/weather?q=\(city)&units=imperial&APPID=d738fda6eb94c9be97868ba69fd0fdbd")!
+            
+            let weatherResource = Resource<Any>(url: weatherURL) { data in
+                return data
+            }
+            
+            Webservice().load(resource: weatherResource) { result in
+                
+            }
+            
+            
+        }
+        
     }
     
     @IBAction func close() {
