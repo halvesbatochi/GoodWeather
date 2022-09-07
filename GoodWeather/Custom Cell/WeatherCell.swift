f6955d048d834337376c7ac0db2e1f8f274c9010
Henrique Alves Batochi <32850493+halvesbatochi@users.noreply.github.com>
2022-09-07 19:53:03 -0300

    Custom Cell

---
+++ GoodWeather/Custom Cell/WeatherCell.swift
@@ -0,0 +1,19 @@
+//
+//  WeatherCell.swift
+//  GoodWeather
+//
+//  Created by Henrique Alves Batochi on 07/09/22.
+//
+
+import Foundation
+import UIKit
+
+class WeatherCell: UITableViewCell {
+    
+    @IBOutlet weak var cityNameLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    
+    
+    
+    
+}
